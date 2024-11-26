a437d94b7b721b3ad9626c2325af4b12632d9142
snyder-io <tushar.whitecollar@gmail.com>
2024-11-26 18:23:00 +0530

    finetuning the response

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -8,20 +8,35 @@ export const signup = async (req, res) => {
     try {
         // Input validations
         if (!fullName) {
-            return res.status(400).send("fullName is required")
+            return res.status(400).json({
+                status: res.statusCode,
+                message: "fullName is required"
+            })
         }
         if (!email) {
-            return res.status(400).send("email is required")
+            return res.status(400).json({
+                status: res.statusCode,
+                message: "email is required"
+            })
         }
         if (!password) {
-            return res.status(400).send("password is required")
+            return res.status(400).json({
+                status: res.statusCode,
+                message: "password is required"
+            })
         }
         if (password.length < 6) {
-            return res.status(400).send("password must be at least 6 characters")
+            return res.status(400).json({
+                status: res.statusCode,
+                message: "password must be at least 6 characters"
+            })
         }
         const user = await User.findOne({ email });
         if (user) {
-            return res.status(400).send("User already exists")
+            return res.status(400).json({
+                status: res.statusCode,
+                message: "User already exists"
+            })
         }
         // hash password
         const salt = await bcrypt.genSalt(10);
@@ -38,25 +53,27 @@ export const signup = async (req, res) => {
             await newUser.save();
             res.status(201).json(
                 {
-                    _id: newUser._id,
-                    fullName: newUser.fullName,
-                    email: newUser.email,
-                    profileImage: newUser.profileImage,
+                    status: res.statusCode,
+                    message: "User created successfully",
+                    user: {
+                        _id: newUser._id,
+                        fullName: newUser.fullName,
+                        email: newUser.email,
+                        profileImage: newUser.profileImage,
+                    }
                 }
             )
 
         } else {
-            res.status(400).send("Invalid user data")
+            res.status(400).json({
+                message: "Invalid user data"
+            })
         }
-
-
     } catch (error) {
         console.log("Error in signup controller", error.message);
-        res.status(500).json(
-            {
-                message: "Intermal Server Error"
-            }
-        )
+        res.status(500).json({
+            message: "Intermal Server Error"
+        })
     }
 };
 export const login = (req, res) => {
