d0e7a9b8ae73c03fac9b33357dee0c3aa6c74b89
snyder-io <tushar.whitecollar@gmail.com>
2024-12-01 20:12:32 +0530

    add message route

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ dotenv.config();
 import cookieParser from "cookie-parser";
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import { connectDB } from "./libs/db.js";
 const PORT = process.env.PORT
 
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/message", messageRoutes)
 
 app
     .listen(PORT, () => {
